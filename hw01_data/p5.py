1f683086d7685c5955a89032ff091b824605622e
Jonas Rudolph <jonas.rudolph@t-online.de>
2022-04-27 13:56:27 +0200

    Upload 5.2

--- hw01_data/p5.py
+++ hw01_data/p5.py
@@ -9,7 +9,7 @@ vector_dim = 100
 def dataset_reader(dataset):
     file = open(dataset, mode='r')
     file_length = sum(1 for line in file)
-    data = np.empty((file_length, vector_dim))
+    data = np.empty((file_length, vector_dim + 1))
     labels = np.empty((file_length,1))
     file.seek(0)
 
@@ -20,6 +20,7 @@ def dataset_reader(dataset):
         else:
             labels[i] = 0
         vec = vector.split()
+        vec.append(1)
 
         for x, empty_vec in enumerate(data[i]):
             data[i][x] = vec[x]
@@ -28,11 +29,51 @@ def dataset_reader(dataset):
 # ----------------------------------------------------------------------------------------------------------------------
 #               5.2 NUMPY IMPLEMENTATION
 # ----------------------------------------------------------------------------------------------------------------------
+def split_batches(data, labels, batch_size):
+    # permutate the arrays simultaneously and split them in batches of equal sizes
+    permutation = np.random.permutation(len(labels))
+    X = data[permutation]
+    Y = labels[permutation]
+    X = np.array_split(X, len(labels)/batch_size)
+    Y = np.array_split(Y, len(labels)/batch_size)
+    return X, Y
 
+def sigmoid(x):
+    return 1.0 / (1 + np.exp(-x))
+
+def train_perceptron(initial_w, train_x, train_y, epochs, batch_size, learning_rate):
+
+    def epoch(train_x, train_y, w_input, batch_size, learning_rate):
+        train_x_batches, train_y_batches = split_batches(train_x, train_y, batch_size)
+
+        for x_batch, y_batch in zip(train_x_batches, train_y_batches):
+            sum = 0
+            for x, y in zip(x_batch, y_batch):
+                sigm = sigmoid(np.dot(w_input, x))
+                sum += (sigm-y) * sigm * (1 - sigm) * x
+            w_input = w_input - (learning_rate/len(y_batch)) * sum
+        return w_input
+
+    for i in range(epochs):
+        trained_w = epoch(train_x, train_y, initial_w, batch_size, learning_rate)
+
+        print(trained_w)
+    return
+
+def evaluate_perceptron():
+
+    return
 # ----------------------------------------------------------------------------------------------------------------------
 #               5.3 TRAINING
 # ----------------------------------------------------------------------------------------------------------------------
+def run_perceptron():
+    data, labels = dataset_reader('DATA/rt-polarity.dev.vecs')
+    w = np.random.normal(0, 1, (vector_dim + 1))
+    print('Run perceptron with initial parameters: ')
+    train_perceptron(w, data, labels, epochs=50, batch_size=10, learning_rate=0.01)
+
+
 
 
 if __name__ == "__main__":
-    dataset_reader('DATA/rt-polarity.dev.vecs')
+    run_perceptron()
